86624f26d906723ac1948984180a052433578cf3
Simone GG <simonegill@clearbids.,uk>
2015-12-14 19:20:17 +0530

    treials are a bum

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -9,7 +9,7 @@ class ChargesController < ApplicationController
 						    :source  => params[:stripeToken]
 						  )
 
-						  ## Check and confirm with Stripe the following information
+						 
 
 						  charge = Stripe::Charge.create(
 						    :customer    => customer.id,
@@ -18,12 +18,7 @@ class ChargesController < ApplicationController
 						    :currency    => 'gbp'
 						  )
 
-						  #NOTE:# Once Purchase has been reccieved and checked take data and throw it into the the database
-						  #NOTE:# The placement of this information into the database equals the creation of an order!!
-
-						  # Once information is all checked put this information into a local database 
-						  #to be used for data analysis later
-
+						 
 						   purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], 
     amount: params[:amount], description: charge.description, currency: charge.currency,
     customer_id: customer.id, product_id: 1, uuid: SecureRandom.uuid)
@@ -31,8 +26,7 @@ class ChargesController < ApplicationController
 redirect_to purchase
 
 
-						####### IN CASE OF ERROR RUN THE FOLLOWINg
-						########## WE ARE RUNNING A STRIPE FUNCTION SO THIS IS A STRIPE DEFAULT!
+						
 						rescue Stripe::CardError => e
 						  flash[:error] = e.message
 						  redirect_to new_charge_path
@@ -40,3 +34,13 @@ redirect_to purchase
 
 
 end
+
+ ## Check and confirm with Stripe the following information
+
+  #NOTE:# Once Purchase has been reccieved and checked take data and throw it into the the database
+						  #NOTE:# The placement of this information into the database equals the creation of an order!!
+
+						  # Once information is all checked put this information into a local database 
+						  #to be used for data analysis later
+####### IN CASE OF ERROR RUN THE FOLLOWINg
+						########## WE ARE RUNNING A STRIPE FUNCTION SO THIS IS A STRIPE DEFAULT!
