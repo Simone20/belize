6d1a11b65f6356aebfe8ae180b3df05bfc3970cc
Simone GG <simonegill@clearbids.,uk>
2015-12-14 19:34:06 +0530

    still in fix mode

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,20 +1,15 @@
 class Purchase < ActiveRecord::Base
-
-	after_create :email_purchaser
+   after_create :email_purchaser
 		
-
-		def to_param
-		 uuid
-		end
+	def to_param
+	  uuid
+	end
 		
+	private
 
-
-
-private
-
-def email_purchaser
-   PurchaseMailer.purchase_receipt(self).deliver
-end
+	def email_purchaser
+	   PurchaseMailer.purchase_receipt(self).deliver
+	end
 
 
 
@@ -23,8 +18,4 @@ end
 
 
 
-		# AS THE STORING OF INFOR~MATION IN LOCAL DB(STRIPE HAS CHECKED AND ~VALIDATED THAT THE CHARGE IS CORRECT)
-		# WE WANT TO CREATE A FUNCTION AS SOON AS CHARGE IS CREATED 
-		#  MAILURE FUNCTION BELOW UUID
-
-# WE ARE CALLING A MAILER BUT DOES IT EXIST FOR CPU RECALL?
+	
